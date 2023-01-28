fb8da215294ab0dceaecf91ad6fec7ede757f9bf
Russell Coleman <rootofsilence@gmail.com>
2023-01-28 17:36:33 +0000

    auto generate buttons

--- js/script.js
+++ js/script.js
@@ -10,4 +10,17 @@ if (localStorage.getItem("weatherAppStored") === null) {
 
 //create variable to hold local storage array
 var retreivedCities = JSON.parse(window.localStorage.getItem("weatherAppStored"));
-console.log(retreivedCities)
+//console.log(retreivedCities)
+
+//render buttons
+for (i = 0; i < retreivedCities.length; i++) {
+    //console.log(i)
+    var displayCities = (retreivedCities[i]);
+    //console.log(displayCities)
+  
+    //append cities buttons
+    $("aside").append(
+      ` <p><button class="btn btn-sm btn-primary" data-name ="${displayCities}">${displayCities}
+          </button></p>`
+    );
+  }
