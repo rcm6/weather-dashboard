841fbeadc94cb2758f2d4f25db1e92cacf6e54a7
Russell Coleman <rootofsilence@gmail.com>
2023-01-28 17:50:51 +0000

    added first API call

--- js/script.js
+++ js/script.js
@@ -24,3 +24,47 @@ for (i = 0; i < retreivedCities.length; i++) {
           </button></p>`
     );
   }
+
+
+  //search weather from button click or search
+
+  $("aside").on("click", function() {
+    event.preventDefault()
+      var location = ($(this).attr("data-name"));
+      //var searchLocation = ($("input").val());
+
+location = "london"
+
+        console.log(location);
+
+
+
+    var APIKey = "c90265758d6d690ed02c2c3f3028ca77";
+      var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + location + "&appid=" + APIKey;
+      
+      // ajax call for summary
+      $.ajax({
+        url: queryURL,
+        method: "GET"
+      }).then(function(response) {
+    console.log(response)
+    var conTemp = parseFloat(response.main.temp) - 273.15
+    // apend today summary div
+      $("#today").append(
+        `<div><b>${(response.name)}</b></div>
+        <div>Temp: ${(conTemp.toFixed(2))}°C</b></div>
+        <div>Wind: ${(response.wind.speed)}</div>
+        <div>Humidity: ${(response.main.humidity)}</div>`
+      );
+
+      var lat = (response.coord.lat);
+      var lon = (response.coord.lon);
+     
+
+   
+
+    })
+      
+    })
+  
+
